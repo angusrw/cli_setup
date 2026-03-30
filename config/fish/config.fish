462c54217d794c8d7a52c71765bf56edd6634d13
Angus R. Williams <angusrwilliams@gmail.com>
2026-03-30 19:04:39 +0100

    update fish and zellij layouts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
-\# PATH
+# PATH
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 
@@ -7,8 +7,9 @@ set -gx HOMEBREW_CASK_OPTS "--appdir=~/Apps"
 
 # Aliases
 alias uvenv="source .venv/bin/activate.fish"
-alias claude_apm="claude --append-system-prompt (cat ~/.claude/auto_plan_mode.txt)"
+alias pybase="source ~/python_base/.venv/bin/activate.fish"
 
+alias claude_apm="claude --append-system-prompt (cat ~/.claude/auto_plan_mode.txt)"
 
 # Starship prompt
 set -gx STARSHIP_CONFIG "$HOME/.config/starship/mytheme.toml"
@@ -19,4 +20,5 @@ if status is-interactive
     and not set -q ZELLIJ
     exec zellij
 end
+
 source ~/.safe-chain/scripts/init-fish.fish # Safe-chain Fish initialization script
